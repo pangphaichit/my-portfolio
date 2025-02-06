d045d3970d0b6ecb74c8d1efb25345161d48703e
pangphaichit <pg_grj@hotmail.com>
2025-02-06 21:03:02 +0700

    feat: create homepage for portfolio

---
+++ src/components/AboutPage.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="about-container">
+    <NavBar />
+    <div></div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+import NavBar from '@/components/NavBar.vue';
+
+export default defineComponent({
+  name: 'AboutPage',
+  components: {
+    NavBar,
+  },
+});
+</script>
+
+<style scoped>
+.about-container {
+  position: relative;
+  width: 100%;
+  height: 100vh;
+  font-family: 'Raleway', sans-serif;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  background-image: repeating-radial-gradient(circle at 0 0, transparent 0, #e5e5f7 30px),
+    repeating-linear-gradient(#ffb5b58a, #ffb8b8);
+}
+</style>
---
+++ src/components/ContactPage.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="contact-container"><NavBar /></div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+import NavBar from '@/components/NavBar.vue';
+
+export default defineComponent({
+  name: 'ContactPage',
+  components: {
+    NavBar,
+  },
+});
+</script>
+
+<style scoped>
+.contact-container {
+  position: relative;
+  width: 100%;
+  height: 100vh;
+  font-family: 'Raleway', sans-serif;
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  background: rgb(255, 245, 254);
+  background: linear-gradient(180deg, rgb(174, 193, 227), rgb(246, 206, 218));
+}
+</style>
