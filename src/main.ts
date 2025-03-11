60c6c2c61af1c8d168224e659c5c52da7aea94b8
pangphaichit <pg_grj@hotmail.com>
2025-03-11 17:19:48 +0700

    feat: update FooterApp component with social links and footer styling

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,26 @@
 import { createApp } from 'vue';
+import Toast, { PluginOptions } from 'vue-toastification';
+import 'vue-toastification/dist/index.css';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import App from './App.vue';
 import router from './router';
 
-createApp(App).use(router).mount('#app');
+library.add(faGithub, faLinkedin);
+
+const options: PluginOptions = {
+  position: 'bottom-right' as PluginOptions['position'],
+  timeout: 3000,
+  closeOnClick: true,
+  pauseOnHover: true,
+  draggable: true,
+};
+
+const app = createApp(App);
+
+app.component('font-awesome-icon', FontAwesomeIcon);
+
+app.use(router);
+app.use(Toast, options);
+app.mount('#app');
